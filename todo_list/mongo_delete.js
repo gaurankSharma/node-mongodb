61dba20d0fb434e6b55a8bbe4f497c8a68910055
Gaurank Sharma <gaurank@cartoonmango.com>
2019-04-09 11:28:23 +0530

    delete method included

---
+++ todo_list/mongo_delete.js
@@ -0,0 +1,32 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
+  if (err) {
+    return console.log("unable to connect to database");
+  }
+  console.log("connected to the server");
+  const db = client.db("TodoApp");
+
+  // delete many item at once
+  //   db.collection("Todos")
+  //     .deleteMany({ name: "gaurank" })
+  //     .then(result => {
+  //       console.log(result);
+  //     });
+
+  //delete one
+  //   db.collection("Todos")
+  //     .deleteOne({ completed: false })
+  //     .then(result => {
+  //       console.log(result);
+  //     });
+
+  //find one and delete and get its value back
+  db.collection("Todos")
+    .findOneAndDelete({ complete: false })
+    .then(result => {
+      console.log(result);
+    });
+
+  //  client.close();
+});
