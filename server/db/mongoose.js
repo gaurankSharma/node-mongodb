4f128d7ff1d091682e696aeb57c4049c62453485
Gaurank Sharma <gaurank@cartoonmango.com>
2019-04-11 11:12:03 +0530

    database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(
+  "mongodb+srv://gaurank24:garuank@todoapp-5hpih.mongodb.net/test?retryWrites=true/TodoApp"
+);
 
 module.exports = {
   mongoose: mongoose
