1b81ff08ca002cd1d0ea4b0ea1f32f8d024ecf2b
Gaurank Sharma <gaurank@cartoonmango.com>
2019-04-12 14:19:50 +0530

    auth added in middelewear

--- server/server.js
+++ server/server.js
@@ -71,6 +71,27 @@ app.post("/user", (req, res) => {
     });
 });
 
+let authenticate = (req, res, next) => {
+  let token = req.header("x-auth");
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+      req.user = user;
+      req.token = token;
+      next();
+    })
+    .catch(e => {
+      res.status(401).send();
+    });
+};
+
+app.get("/user/me", authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.listen(port, () => {
   console.log(`the port is ${port}`);
 });
